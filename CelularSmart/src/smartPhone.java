5c7f96f779aa4b7c0138c08b6a4a433192c132e3
TiagoDutra91 <pavanati.tiago@gmail.com>
2024-07-15 00:48:15 -0300

    novo comit

--- CelularSmart/src/smartPhone.java
+++ CelularSmart/src/smartPhone.java
@@ -1,15 +1,53 @@
+import NavegadorInternet.adicionarNovaAba;
+import NavegadorInternet.atualizarPagina;
+import NavegadorInternet.exibirPagina;
+import NavegadorInternet.navegadorInternet;
+import ReprodutorMusical.pausar;
 import ReprodutorMusical.reprodutorMusical;
+import ReprodutorMusical.selecionarMusica;
+import ReprodutorMusical.tocar;
+import Telefone.atender;
+import Telefone.iniciarCorreioVoz;
+import Telefone.ligar;
 import Telefone.telefone;
 
-public class smartPhone implements telefone,reprodutorMusical{
+public class smartPhone {
+    public static void main(String[] args) {
+    telefone celular = new ligar();
+    telefone celularAtender = new atender();
+    telefone celularVoz = new iniciarCorreioVoz();
+       
+        celular.AppTelefone();
+        celularAtender.AppTelefone();
+        celularVoz.AppTelefone();
 
-    public void AppTelefone(){
+        System.out.println("\n _________________");
     
-    }
- 
-    public void AppMusica(){
-    }
+    reprodutorMusical player = new tocar();
+    reprodutorMusical playerPause = new pausar();
+    reprodutorMusical playerSelecionar = new selecionarMusica();
 
-    public void AppNavegador(){
-    }
+    player.AppMusica();
+    playerPause.AppMusica();
+    playerSelecionar.AppMusica();
+
+        System.out.println("\n____________________");
+
+    navegadorInternet web = new exibirPagina();
+    navegadorInternet webAtualizar = new atualizarPagina();
+    navegadorInternet webNovaAba = new adicionarNovaAba();
+
+    web.AppNavegador();
+    webNovaAba.AppNavegador();
+    webAtualizar.AppNavegador();
+
+
+
+
+    
+    
+
+
+    
+}
 }
